2c3537e51bb69c2f0c058aeaf20e349e30412ead
dangluan <peter@rubify.com>
2013-06-10 20:10:33 +0700

    fix for gem

--- app/helpers/gmpoint/gmpoint_helper.rb
+++ app/helpers/gmpoint/gmpoint_helper.rb
@@ -8,14 +8,14 @@ module Gmpoint::GmpointHelper
     
     # object = model_name.to_s.singularize.classify.constantize.find(params[:id])
     object = model
-    model_name = model.class.to_s.singularize
+    model_name = model.class.to_s.downcase
     @address = object.location_address
     @latitude = object.location_latitude
     @longitude = object.location_longitude
     opts = {allow: 'show', searchbox: 'show', width: 800, height: 400, latitude: "#{@latitude}", longitude: "#{@longitude}", address: "#{@address}", zoom: 13, style: "border: 1px solid #green;", searchbox_width: 400}.merge(opts)
     [
       content_tag(:div, :id => "geopoint_search_box_container", style: " display: #{opts[:searchbox]}") do
-        tag(:input, type: :text, placeholder: 'Search', id: "gmpoint_#{model_name}_search_box", style: "width: #{opts[:searchbox_width]}px; border:1px solid #{:color};")
+        tag(:input, type: :text, placeholder: 'Search', id: "gmpoint_#{model_name}_search_box", style: "")
       end,      
       content_tag(:div, '', id: "map_canvas", style: "width: #{opts[:width]}px; height: #{opts[:height]}px; #{opts[:style]}"),
       content_tag(:div, '', class: "data-location", data: {model: model_name, address:opts[:address], latitude: opts[:latitude] , longitude: opts[:longitude], zoom: opts[:zoom]}),
